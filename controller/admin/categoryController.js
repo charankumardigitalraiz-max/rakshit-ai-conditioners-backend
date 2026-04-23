f4a5a7b9033bda0c26d5dd511926c34e811b18cf
charankumardigitalraiz-max <charankumardigitalraiz@gmail.com>
2026-04-23 13:43:30 +0530

    category controller updated

--- controller/admin/categoryController.js
+++ controller/admin/categoryController.js
@@ -1,4 +1,4 @@
-const Category = require('../../models/category');
+const Category = require('../../models/Category');
 const ErrorResponse = require('../../utils/errorResponse');
 const asyncHandler = require('../../middleware/asyncHandler');
 // Get all categories
