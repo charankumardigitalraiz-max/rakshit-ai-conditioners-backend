913d5ce6d390854b16a8f143ae8d3ef78c80cd21
charankumardigitalraiz-max <charankumardigitalraiz@gmail.com>
2026-04-25 19:45:57 +0530

    category uploaded sucessfully

--- controller/admin/categoryController.js
+++ controller/admin/categoryController.js
@@ -5,7 +5,7 @@ const asyncHandler = require('../../middleware/asyncHandler');
 exports.getCategories = asyncHandler(async (req, res, next) => {
     // const page = parseInt(req.query.page, 10) || 1;
     // const limit = parseInt(req.query.limit, 10) || 10;
-    const startIndex = (page - 1) * limit;
+    // const startIndex = (page - 1) * limit;
 
     let query = {};
 
